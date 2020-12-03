e77f74ec142ece2196027cc7d45704a66a464b40
faisalhatila <hatilas1994@gmail.com>
2020-12-03 19:07:17 +0500

    about us done

--- src/Main/Components/MainSite/0MainPage.js
+++ src/Main/Components/MainSite/0MainPage.js
@@ -10,73 +10,76 @@ import Contact from "./Contact";
 const PortfolioSection = React.lazy(() => import("../Pages/Portfolio"));
 const AboutSection = React.lazy(() => import("../Pages/About"));
 
-const PortfolioGrid = React.lazy(() => import("../../../shared/components/UIElements/PortfolioGrid"));
+const PortfolioGrid = React.lazy(() =>
+  import("../../../shared/components/UIElements/PortfolioGrid")
+);
 
 const MainPage = (props) => {
-    const { exploreState, exploreFunctionHandler } = props;
+  const { exploreState, exploreFunctionHandler } = props;
 
-    const transformScroll = (event) => {
-        // Change Here -> ! -> ''
-        if (exploreState) {
-            if (event.deltaY < 0) {
-                window.scrollBy(0, -500);
-                return;
-            }
-            window.scrollBy(0, 500);
-            event.preventDefault();
-            return;
-        }
-        if (event.deltaY < 0) {
-            !exploreState && window.scrollBy(-500, 0);
-            return;
-        }
-        !exploreState && window.scrollBy(500, 0);
-        event.preventDefault();
-    };
-    var element = document.scrollingElement || document.documentElement;
-    element.addEventListener("wheel", transformScroll);
-
-    let content;
-
+  const transformScroll = (event) => {
+    // Change Here -> ! -> ''
     if (!exploreState) {
-        content = (
-            <div className={classes.MainPage}>
-                <Home exploreFunctionHandler={exploreFunctionHandler} />
-                <Services exploreFunctionHandler={exploreFunctionHandler} />
-                <About exploreFunctionHandler={exploreFunctionHandler} />
-                <Portfolio exploreFunctionHandler={exploreFunctionHandler} />
-                <Careers exploreFunctionHandler={exploreFunctionHandler} />
-                <Contact exploreFunctionHandler={exploreFunctionHandler} />
-            </div>
-        );
+      if (event.deltaY < 0) {
+        window.scrollBy(0, -500);
+        return;
+      }
+      window.scrollBy(0, 500);
+      event.preventDefault();
+      return;
     }
-
-    if (exploreState === "portfolio") {
-        content = (
-            <div className={classes.MainPage}>
-                <PortfolioSection />
-            </div>
-        );
+    if (event.deltaY < 0) {
+      !exploreState && window.scrollBy(-500, 0);
+      return;
     }
+    !exploreState && window.scrollBy(500, 0);
+    event.preventDefault();
+  };
+  var element = document.scrollingElement || document.documentElement;
+  element.addEventListener("wheel", transformScroll);
 
-    if (exploreState === "about") {
-        content = (
-            <div className={classes.MainPage}>
-                <AboutSection />
-            </div>
-        );
-    }
+  let content;
 
-    // Change Here
+  if (!exploreState) {
+    content = (
+      <div className={classes.MainPage}>
+        <Home exploreFunctionHandler={exploreFunctionHandler} />
+        <Services exploreFunctionHandler={exploreFunctionHandler} />
+        <About exploreFunctionHandler={exploreFunctionHandler} />
+        <Portfolio exploreFunctionHandler={exploreFunctionHandler} />
+        <Careers exploreFunctionHandler={exploreFunctionHandler} />
+        <Contact exploreFunctionHandler={exploreFunctionHandler} />
+      </div>
+    );
+  }
 
-    // content = (
-    //     <div className={classes.MainPage}>
-    //         <PortfolioGrid />
-    //         {/* <PortfolioSection /> */}
-    //     </div>
-    // );
+  if (exploreState === "portfolio") {
+    content = (
+      <div className={classes.MainPage}>
+        <PortfolioSection />
+      </div>
+    );
+  }
 
-    return content;
+  if (exploreState === "about") {
+    content = (
+      <div className={classes.MainPage}>
+        <AboutSection />
+      </div>
+    );
+  }
+
+  // Change Here
+
+  content = (
+    <div className={classes.MainPage}>
+      <AboutSection />
+      {/* <PortfolioGrid /> */}
+      {/* <PortfolioSection /> */}
+    </div>
+  );
+
+  return content;
 };
 
 export default MainPage;
