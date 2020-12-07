a428666e67461a36c4badc7e6faf5b30eebfedfc
Muzammil <sma3797@gmail.com>
2020-12-07 10:49:51 +0500

    UI Completed

--- src/Main/Components/MainSite/Contact.js
+++ src/Main/Components/MainSite/Contact.js
@@ -1,7 +1,28 @@
 import React from "react";
 import classes from "./Contact.module.css";
 
+import { useHttpClient } from "../../../shared/hooks/http-hook";
+
 const Contact = (props) => {
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+    const submitContactFormHandler = async () => {
+        console.log("submitContactFormHandler", process.env.REACT_APP_BACKEND_URL);
+        try {
+            const responseData = await sendRequest(
+                `${process.env.REACT_APP_BACKEND_URL}/check`,
+                // "POST",
+                // {
+                //     "Content-Type": "application/json",
+                // },
+                // JSON.stringify({}),
+                // // data,
+            );
+            console.log("responseData", responseData);
+        } catch (error) {}
+    };
+    error && console.log("error", error);
+
     return (
         <div id="contact" className={classes.Home}>
             <div className={classes.HomeWidth}>
@@ -27,7 +48,7 @@ const Contact = (props) => {
                             <div className={classes.FormElement}>
                                 <textarea placeholder="Type Your Message" />
                             </div>
-                            <a>Submit</a>
+                            <a onClick={() => submitContactFormHandler()}>Submit</a>
                         </div>
                     </div>
                 </div>
